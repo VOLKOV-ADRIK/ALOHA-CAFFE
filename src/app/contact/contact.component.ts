e5647278717ae159bd502e7c8ab3c25996807016
ADRIK <124713189+VOLKOV-ADRIK@users.noreply.github.com>
2023-03-23 11:43:18 -0500

    Pequeños cambios

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,4 +9,16 @@ import { CargarscripsService } from '../cargarscrips.service';
 })
 export class ContactComponent {
 
+  constructor(private cargarscripts: CargarscripsService){
+    cargarscripts.cargar([
+      "assets/vendor/aos/aos.js",
+      "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+      "assets/vendor/glightbox/js/glightbox.min.js",
+      "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+      "assets/vendor/swiper/swiper-bundle.min.js",
+      "assets/vendor/php-email-form/validate.js",
+      "assets/js/main.js"
+    ])
+  }
+
 }
